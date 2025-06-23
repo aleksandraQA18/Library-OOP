907ce04aea8363e4363eb7c9b32a74b17a3ae573
aleksandraQA18 <pythonpractice393@gmail.com>
2025-06-23 20:13:51 +0200

    add validation to reader init method

--- reader.py
+++ reader.py
@@ -1,18 +1,40 @@
-import uuid
+import string
+from random import choice, randint
+
+
+class ValidationError(Exception):
+    pass
 
 
 class Reader:
 
-    def __init__(self, first_name, last_name):
-        self.id = str(uuid.uuid4())
-        self.first_name = first_name
-        self.last_name = last_name
-        # self.books have a book object and a return date
-        self.books = []
+    def __init__(self, first_name: str, last_name: str, username: str, email: str):
+        self.card_no = self.generate_card_no()
+        self.first_name = self.input_validation(first_name)
+        self.last_name = self.input_validation(last_name)
+        self.username = self.input_validation(username)
+        self.email = self.email_validation(email)
 
-    def print_reader_books(self):
-        if not self.books:
-            print("You don't currently have any books on loan")
-        else:
-            for book in self.books:
-                print(book.title)
+    def input_validation(self, field: str) -> str:
+        if len(field) < 3 or not field.strip():
+            raise ValidationError(
+                "First name, last name and username must be at least 3 characters long."
+            )
+        return field
+
+    def email_validation(self, email: str) -> str:
+        if len(email) < 3 or not email.strip():
+            raise ValidationError("Email must be at least 3 characters long.")
+        if "@" not in email:
+            raise ValidationError('email should contain "@"')
+        return email
+
+    def generate_card_no(self) -> str:
+        """
+        Generates a random card number for the reader.
+        Format: 4 digits + uppercase letter / uppercase letter + 2 digits (e.g., 1234A/B56)
+        """
+        letters = string.ascii_uppercase
+        return (
+            f"{randint(1000, 9999)}{choice(letters)}/{choice(letters)}{randint(10, 99)}"
+        )
